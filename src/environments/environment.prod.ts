2cae39bbcc76380c1bba5a85988bb3a2963f1ba0
Enrique <enrique.rojas@riabc.net>
2025-12-05 16:44:03 -0500

    habilitacion de https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    baseUrl: 'http://199.89.55.49/apitwh3',
-    socketUrl: 'http://199.89.55.49/apitwh3', // URL base para SignalR Hub (el servicio construirá la URL completa: {socketUrl}/hubs/ordenSalida)
+    baseUrl: 'https://twh.pe/apitwh3',
+    socketUrl: 'https://twh.pe/apitwh3', // URL base para SignalR Hub (el servicio construirá la URL completa: {socketUrl}/hubs/ordenSalida)
     enableWebSocket: true // Habilitar/deshabilitar SignalR (poner false si el servidor aún no está configurado)
 };
